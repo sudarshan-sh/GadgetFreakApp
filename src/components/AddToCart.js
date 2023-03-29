46faafedef475af482edb410cd149a50e068c43d
Sudarshan Sharma <mail2sudarshan.sharma@gmail.com>
2023-03-29 22:53:40 +0530

    ready for deployment

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -59,7 +59,7 @@ const AddToCart = ({ product }) => {
 
         <Link
           to='/cart'
-          className='btn'
+          className='btn add-to-cart'
           onClick={() => addToCart(id, mainColor, amount, product)}
         >
           add to cart
@@ -111,6 +111,9 @@ const Wrapper = styled.section`
   .btn {
     margin-top: 1rem;
     width: 140px;
+  }
+  .add-to-cart{
+    text-align: center;
   }
 `
 export default AddToCart
